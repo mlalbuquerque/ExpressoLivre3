15f97787ab5160e491efd6f5b7e06959aa0067e9
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-11-20 15:20:29 -0200

    Resolving conflict with master (Removing debug lines!)

--- tine20/Messenger/js/FileTransfer.js
+++ tine20/Messenger/js/FileTransfer.js
@@ -78,6 +78,7 @@ Tine.Messenger.FileTransfer = {
         var app = Tine.Tinebase.appMgr.get('Messenger');
         var from = $(msg).attr('from'),
             jid = Strophe.getBareJidFromJid(from),
+<<<<<<< HEAD
             file = $(msg).find('file'),
             fileName = file.attr('name'),
             fileSize = file.attr('size'),
@@ -136,6 +137,21 @@ Tine.Messenger.FileTransfer = {
                         callbackSend();
                     }
                 });
+=======
+            to = $(msg).attr('to'),
+            file = $(msg).find('file');
+        
+        Ext.MessageBox.buttonText.yes = _('Allow');
+        Ext.MessageBox.buttonText.no = _('Deny');
+        Ext.MessageBox.minWidth = 450;
+        Ext.MessageBox.confirm(_('File Transfer'),
+            jid + ' ' + _('wants to send you a file:') +
+                '<h6 style="padding: 5px 0 0 0;">' + file.attr('name') + 
+                ' (' + file.attr('size') + ' bytes)</h6>',
+            function (id) {
+                var filePath = file.attr('path') + file.attr('name');
+                $('#iframe-download').attr('src', '/download.php?file=' + filePath + '&download=' + id);
+>>>>>>> Removing debug lines!
             }
 
             var resources = new Ext.Window({
