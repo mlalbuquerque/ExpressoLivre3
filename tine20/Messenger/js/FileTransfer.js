36f06ff4050b039ff909116558ef9a1b128e0371
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-11-20 15:22:23 -0200

    Resolving conflict with master (Removing debug lines!)

--- tine20/Messenger/js/FileTransfer.js
+++ tine20/Messenger/js/FileTransfer.js
@@ -9,7 +9,7 @@ Tine.Messenger.FileTransfer = {
         var to = typeof item == 'string' ? item : item.node.attributes.jid,
             iFrame = $('#iframe-upload'),
             inputFile = iFrame.contents().find('#sendfile');
-
+        
         inputFile.click();
         inputFile.one('change', function () {
             var form = $(this).parent('form');
@@ -78,7 +78,6 @@ Tine.Messenger.FileTransfer = {
         var app = Tine.Tinebase.appMgr.get('Messenger');
         var from = $(msg).attr('from'),
             jid = Strophe.getBareJidFromJid(from),
-<<<<<<< HEAD
             file = $(msg).find('file'),
             fileName = file.attr('name'),
             fileSize = file.attr('size'),
@@ -137,21 +136,6 @@ Tine.Messenger.FileTransfer = {
                         callbackSend();
                     }
                 });
-=======
-            to = $(msg).attr('to'),
-            file = $(msg).find('file');
-        
-        Ext.MessageBox.buttonText.yes = _('Allow');
-        Ext.MessageBox.buttonText.no = _('Deny');
-        Ext.MessageBox.minWidth = 450;
-        Ext.MessageBox.confirm(_('File Transfer'),
-            jid + ' ' + _('wants to send you a file:') +
-                '<h6 style="padding: 5px 0 0 0;">' + file.attr('name') + 
-                ' (' + file.attr('size') + ' bytes)</h6>',
-            function (id) {
-                var filePath = file.attr('path') + file.attr('name');
-                $('#iframe-download').attr('src', '/download.php?file=' + filePath + '&download=' + id);
->>>>>>> Removing debug lines!
             }
 
             var resources = new Ext.Window({
