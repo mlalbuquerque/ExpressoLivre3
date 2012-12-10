86dc88c1fbd3301d389e084f463f13fd6b0ce5a0
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-12-10 16:29:32 -0200

    Task#3996 - Removed upload.html, download.php and transferserver.php files

--- tine20/Messenger/Frontend/Http.php
+++ tine20/Messenger/Frontend/Http.php
@@ -1,18 +1,34 @@
 <?php
+
 class Messenger_Frontend_Http extends Tinebase_Frontend_Http_Abstract
 {
-   protected $_applicationName = 'Messenger';
-   
-   /**
-    * Returns all JS files which must be included for this app
-    *
-    * @return array Array of filenames
-    */
-   public function getJsFilesToInclude()
-   {
-       return array(
-           'Messenger/js/Application.js',
-           'Messenger/js/jquery-1.7.1.min.js'
-       );
-   }
+
+    protected $_applicationName = 'Messenger';
+
+    /**
+     * Returns all JS files which must be included for this app
+     *
+     * @return array Array of filenames
+     */
+    public function getJsFilesToInclude()
+    {
+        return array(
+            'Messenger/js/Application.js',
+            'Messenger/js/jquery-1.7.1.min.js'
+        );
+    }
+
+    public function getFile($name, $tmpfile, $downloadOption)
+    {
+        if ($downloadOption == 'yes') {
+            header('Cache-Control: private, max-age=0');
+            header("Expires: -1");
+            // overwrite Pragma header from session
+            header("Pragma: cache");
+            header('Content-Disposition: attachment; filename="' . $name . '"');
+            readfile($tmpfile);
+        }
+        unlink($tmpfile);
+    }
+
 }
